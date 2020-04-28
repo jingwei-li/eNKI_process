f0ff9567555d9c5097f811de2a699ba01f3927d6
jingwei-li <allhappyliferay@gmail.com>
2020-04-28 17:23:27 +0200

    polish subject-list-generation script

--- scripts/preprocessing/generate_subject_list_BIDS.sh
+++ scripts/preprocessing/generate_subject_list_BIDS.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 BIDS_dir="/data/BnB1/Raw_Data/eNKI_BIDS"
-prefix="sub-A"
-regex=^[0-9]{8}$
+regex=^sub-A[0-9]{8}$
 folders=$(find $BIDS_dir -mindepth 1 -maxdepth 1 -type d)
+
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+out_dir=$(dirname $(dirname $script_dir))
 
 subjects=
 for f in $folders; do
@@ -12,4 +14,9 @@ for f in $folders; do
 		subjects="$subjects $bname"
 	fi
 done
-echo $subjects
+
+out_list="${out_dir}/lists/subjects/subjects_BIDS.txt"
+rm $out_list
+for s in $subjects; do
+    echo $s >> $out_list
+done
