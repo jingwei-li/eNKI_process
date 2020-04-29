14a6a8bededa2ccc3b93d7ac4b549f21b4e061d7
Jingwei  Li <jli@bnbmac37.local>
2020-04-29 13:30:33 +0200

    Add script to count subjects with different sessions and acqusitions

---
+++ scripts/preprocessing/count_per_ses_per_acq.py
@@ -0,0 +1,29 @@
+
+import os, sys
+import numpy as np
+import argparse
+
+script_dir = os.path.realpath(__file__)
+ldir = os.path.abspath(os.path.join(script_dir, os.pardir))
+ldir = os.path.abspath(os.path.join(ldir, os.pardir))
+ldir = ldir + '/lists/'
+
+parser = argparse.ArgumentParser()
+subj_ls_def = scripts_dir + 'subjects/subjects_BIDS.txt'
+parser.add_argument("--subj_ls", default=subj_ls_def, help="subject list")
+
+BIDS_dir_def = '/data/BnB1/Raw_Data/eNKI_BIDS'
+parser.add_argument("--BIDS_dir", default=BIDS_dir_def, help="directory to store BIDS format eNKI subjects")
+
+args = parser.parse_args()
+subj_ls = args.subj_ls
+BIDS_dir = args.BIDS_dir
+
+with f = open(subj_ls, 'r'):
+	subjects = f.read()
+	subjects = subjects.split()
+
+for s in subjects:
+	sub_dir = BIDS_dir + '/' + s + '/'
+	sessions = os.walk('.').next()[1]
+	print sessions
